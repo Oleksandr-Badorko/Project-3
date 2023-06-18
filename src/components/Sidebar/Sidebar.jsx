1accc7f2be510b1013f14a5314b9b96954a6cd8b
Badorko Oleksandr <sachabv1999@ukr.net>
2023-06-18 22:41:31 +0300

    Pr-3

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -26,23 +26,23 @@ const Sidebar = () => {
           <i class="ri-home-line"></i>
           <span class="sidebar-span">Main</span>
         </a>
-        <a href="/about" class="br-bt">
+        <a href="/#about" class="br-bt">
           <i class="ri-file-info-line"></i>
           <span class="sidebar-span">About</span>
         </a>
-        <a href="/featured" className="br-bt">
+        <a href="/#featured" className="br-bt">
           <i className="ri-article-line"></i>
           <span className="sidebar-span"> News</span>
         </a>
-        <a href="/team" className="br-bt">
+        <a href="/#team" className="br-bt">
           <i className="ri-team-line"></i>
           <span className="sidebar-span">Team</span>
         </a>
-        <a href="/search" className="br-bt">
+        <a href="/#search" className="br-bt">
           <i className="ri-search-line"></i>
           <span className="sidebar-span">Search</span>
         </a>
-        <a href="/contact" className="br-bt">
+        <a href="/#contact" className="br-bt">
           <i className="far fa-envelope"></i>
           <span className="sidebar-span">Contact</span>
         </a>
