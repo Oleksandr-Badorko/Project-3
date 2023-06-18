6a2072fab5e9f17436fd1e725646e8ff3d535a8a
Badorko Oleksandr <sachabv1999@ukr.net>
2023-06-18 22:23:20 +0300

    Pr-3

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import './reset.scss';
+import "./reset.scss";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createHashRouter } from "react-router-dom";
+import './styles/index.css';
 
-const router = createHashRouter ( routes [
-{
-    path: "/*",
-    element: <App />,
-}
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App router ={router} />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
