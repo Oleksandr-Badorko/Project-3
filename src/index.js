924d86cfd772cabc0338deb67beaf5c63b62aabd
Badorko Oleksandr <sachabv1999@ukr.net>
2023-06-18 22:05:57 +0300

    Pr-3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import "./reset.scss";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import './reset.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createHashRouter } from "react-router-dom";
 
-const router = createHashRouter([
-  {
+const router = createHashRouter ( routes [
+{
     path: "/*",
     element: <App />,
-  },
+}
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App router ={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
