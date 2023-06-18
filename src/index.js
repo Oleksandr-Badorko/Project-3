7ab27640937c7baeaa4ce733ca8dd56fbb82e693
Badorko Oleksandr <sachabv1999@ukr.net>
2023-06-18 22:27:58 +0300

    Pr-3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import "./reset.scss";
+import './reset.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
-import './styles/index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
